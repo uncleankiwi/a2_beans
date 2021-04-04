95c1deeb67a5680b7c7356dac6a07394b6aa85cf
uncleankiwi <feldtspirit@gmail.com>
2021-04-04 13:10:38 -0400

    -

--- src/main/java/com/uc/StoreBean.java
+++ src/main/java/com/uc/StoreBean.java
@@ -1,4 +1,11 @@
 package com.uc;
 
-public class StoreBean {
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+
+@SessionScoped
+@Named
+public class StoreBean implements Serializable {
+
 }
